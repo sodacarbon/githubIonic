e9f3ffdce837eae6f9ae9fb5f47b3d25cba56a0d
sodacarbon <sodacarbon@gmail.com>
2016-07-31 00:52:19 +0700

    SIMPLE SEARCH <ion-searchbar (input)="search($event)"></ion-searchbar> We'll only search when three or more characters have been typed. app/pages/users/user.html

--- app/pages/users/users.ts
+++ app/pages/users/users.ts
@@ -23,7 +23,8 @@ export class UsersPage {
   users: Array<User>;
 
   // Inject the GithubUsers in the constructor of our page component
-  constructor(public nav: NavController, githubUsers: GithubUsers) {
+  // We've added a private key word to the githubUsers in the cnstructor where it is injected as a parameter. This makes the property to be available within the class and be accessible with the this keyword.
+  constructor(public nav: NavController, private githubUsers: GithubUsers) {
     // Test whether the github provider returns data
     githubUsers
       .load()
@@ -31,9 +32,9 @@ export class UsersPage {
 
     // Test if our search function works.
     // quit/ionic serv > Successfully returns array[30] *soda*
-    githubUsers
+    /* githubUsers
       .searchUsers('soda')
-      .then(users => console.log(users));
+      .then(users => console.log(users)); */
   }
 
   // We then add a method that will handle the navigation, goToDetails. It takes in two arguments, the event, just in case we want to do something with the event that triggered the navigation, and the login(username).
@@ -44,5 +45,21 @@ export class UsersPage {
     this.nav.push(UserDetailsPage, { login: login});
   }
 
-
+  // Search for user's from github
+  // Handle input event from search bar
+  search(searchTerm) {
+    let term = searchTerm.target.value;
+    // We will only perform the search if we have 3 or more characters
+    if (term.trim() == '' || term.trim().length < 3) {
+      // Get github users and assign to local user's variable
+      this.githubUsers.load()
+      // Load original users in this case
+        .then(users => this.users = users);
+    }
+    else {
+      // Get the searched users from github
+      this.githubUsers.searchUsers(term)
+        .then(users => this.users = users);
+    }
+  }
 }

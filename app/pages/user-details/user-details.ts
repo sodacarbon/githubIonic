0110ac701e0ac47dee75ba3a026c2509eb012c3d
sodacarbon <sodacarbon@gmail.com>
2016-07-30 21:49:41 +0700

    # Viewing User Details $ ionic g page user-details

---
+++ app/pages/user-details/user-details.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the UserDetailsPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/user-details/user-details.html',
+})
+export class UserDetailsPage {
+
+  constructor(private nav: NavController) {
+
+  }
+
+}
